25c8f3688e2f8aca302236de91e0bdda11c50e99
Peter Kampmann <peter.kampmann@dfki.de>
2012-01-23 18:00:14 +0000

    [lib/NDLCom/include/NDLCom]: follow up to checkin #3686 logic part to store and load udp connection properties
    git-svn-id: https://svn.hb.dfki.de/iStruct/software/trunk@3687 23edfee6-d8fb-8640-80ab-3a9191bda3f1

--- include/NDLCom/udpcom.h
+++ include/NDLCom/udpcom.h
@@ -11,6 +11,7 @@
 
 #include <QObject>
 #include <QDialog>
+#include <QSettings>
 
 #include "ui_udpcom_connect_dialog.h"
 
@@ -74,12 +75,35 @@ namespace NDLCom
             mpUi->setupUi(this);
             connect(mpUi->buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
             connect(mpUi->buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+            connect(mpUi->saveSettings,SIGNAL(clicked()),this, SLOT(writeCommonConnectionSettings()));
+            loadCommonConnectionSettings();
         };
-        ~UdpComConnectDialog(){ };
+        ~UdpComConnectDialog(){ delete mpUi;};
+
+        void loadCommonConnectionSettings(){
+            QSettings *settings = new QSettings("DFKI","NDLCom");
+            mpUi->hostname->addItem(settings->value("network/hostname").toString());
+            mpUi->recvport->addItem(settings->value("network/recvPort").toString());
+            mpUi->sendport->addItem(settings->value("network/sendPort").toString());  
+            mpUi->hostname->setCurrentIndex(mpUi->hostname->count()-1);          
+            mpUi->recvport->setCurrentIndex(mpUi->recvport->count()-1);          
+            mpUi->sendport->setCurrentIndex(mpUi->sendport->count()-1);          
+        }        
 
         QString getHostname() { return mpUi->hostname->currentText(); }
         int getRecvport()     { return mpUi->recvport->currentText().toInt(); }
         int getSendport()     { return mpUi->sendport->currentText().toInt(); }
+
+    public slots:
+        void writeCommonConnectionSettings(){
+    	  QSettings *networkSettings = new QSettings("DFKI","NDLCom");
+	      networkSettings->clear();
+	      networkSettings->setValue("network/hostname",mpUi->hostname->currentText());
+	      networkSettings->setValue("network/recvPort",mpUi->recvport->currentText());
+	      networkSettings->setValue("network/sendPort",mpUi->sendport->currentText());
+	      networkSettings->sync();
+          delete networkSettings;
+        }
 
     private:
         Ui::UdpComConnectDialog* mpUi;
