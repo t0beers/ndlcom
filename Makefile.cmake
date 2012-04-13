c5e39402b6a6c44e4632e15ca601123b27eddbb1
Martin Zenzes <martin.zenzes@dfki.de>
2012-04-13 12:19:03 +0000

    [various projects] renovate cmake completely!!!
    touched a lot! in fact every cmake file, nearly all cmake wrapper makefiles
    and nearly all pkg-config files. but now this buildsystem seems to work nicer!
    
    removed distinction between shared and dynamic libs which was done before.
    it is easier to let cmake decide what to do and switch using "BUILD_SHARED_LIBS".
    see iLeggui/CmakeLists.txt for this. Now, the build speed is doubled since
    only one set has to be compiled. also, pkg-config files can be used by cmake
    to generate a dependency tree from the link-information (since the target has
    the same name as the later lib)
    
    updated the wrapper-makefile in nearly all places (except register, to keep
    old behaviour). has target "dependencys" which draws a dot-file of all linkages
    in the project. nice for debugging. note that clean (yes, distclean too) will
    _only_ delete the local build-directory, nothing more. an example of the
    png generated for iLeggui will be appendend to the svn-mailiing list.
    there is also a "info" target now, which will display where the
    different path's are located.
    
    changed the default location for pkg-config file to "CMAKE_BINARY_DIR". this
    allows us to use one search path for all ${PROJECT}-uninstalled.pc files.
    
    added cmake to math and spuc. eigen2 is missing, but using the version installed
    in the source-tree by the old build-env kinda works. a warning was added.
    
    tried to exclude testgui's from "all" target. if you still find some,
    please tell ;-)
    
    while on the way, i did some minor code changes too, but nothing wild.
    removed an old unfunctional testgui from serialcom. please scream if this is bad.
    
    ah, need no tell, but this works with iLeggui (and widgets). just go to
    gui/iLeggui and type "make -f Makefile.cmake" and a nice symlink will be created
    for you in the gewöhnlichen place. missing CMakelists for iSpineGui can be easily added.
    
    Signed-off-by: Martin Zenzes (RH5) <martin.zenzes@dfki.de>
    
    git-svn-id: https://svn.hb.dfki.de/iStruct/software/trunk@4409 23edfee6-d8fb-8640-80ab-3a9191bda3f1

--- Makefile.cmake
+++ Makefile.cmake
@@ -1,29 +1,51 @@
+# 423d7ddbc -- unique string to find this makefile without sourcecontrol...
 # allows automatical multicore build sessions!
 JOBS=$(shell getconf _NPROCESSORS_ONLN)
-# by default, we use a compiler dependent install place. carefull -- i ask the c++ compiler, not the c-compiler!
-# additionally the environment variable CXX is asked, so not neccessarily the native compiler!
-ARCH=$(shell ${CXX} -dumpmachine)
 
+# we wanna use absolute path' where possible
+SRCDIR=$(shell pwd)
+
+# by default, we use a compiler dependent build and install directory.
+# carefull -- we ask the c++ compiler, not the c-compiler!
+# additionally the environment variable CXX is asked, so not neccessarily the native compiler!
+INSTALLDIR=$(shell readlink -m ~/DFKI.install/$(shell ${CXX} -dumpmachine))
+BUILDDIR=$(shell readlink -m ./build/$(shell ${CXX} -dumpmachine))
+
+# default:
 all: compile
 
 # so we have "build" as a shorthand for creating a new build environment
-build: build/$(ARCH)/Makefile
+build: $(BUILDDIR)/Makefile
 
-build/$(ARCH)/Makefile:
-	mkdir -p build/$(ARCH);\
-	cd build/$(ARCH);\
-	cmake ../../ -DCMAKE_INSTALL_PREFIX=~/DFKI.install/$(ARCH)
+info:
+	@echo "srcdir (here):"
+	@echo $(SRCDIR)
+	@echo "installdir:"
+	@echo $(INSTALLDIR)
+	@echo "builddir:"
+	@echo $(BUILDDIR)
 
-compile: build/$(ARCH)/Makefile
-	${MAKE} -j$(JOBS) -C build/$(ARCH)
+$(BUILDDIR)/Makefile:
+	mkdir -p $(BUILDDIR);\
+	sh -c "cd $(BUILDDIR); cmake $(SRCDIR) -DCMAKE_INSTALL_PREFIX=$(INSTALLDIR)"
+
+dependencys: build
+	mkdir -p $(BUILDDIR)
+	sh -c "cd $(BUILDDIR); cmake $(SRCDIR) --graphviz=dependencys.dot"
+	dot $(BUILDDIR)/dependencys.dot -Tpng > $(SRCDIR)/dependencys.png
+
+compile: build
+	${MAKE} -j$(JOBS) -C $(BUILDDIR)
 
 test: compile
-	${MAKE} -j$(JOBS) -C build/$(ARCH) test
+	${MAKE} -j$(JOBS) -C $(BUILDDIR) test
 
 install: compile
-	${MAKE} -j$(JOBS) -C build/$(ARCH) install
+	${MAKE} -j$(JOBS) -C $(BUILDDIR) install
 
+distclean:
 clean:
-	rm -rf build/$(ARCH)
+	rm -rf $(BUILDDIR)
+	@rm -f dependencys.png
 
 .PHONY: compile
