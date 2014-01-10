680a4e9179cd21534be266a1801f4788a5caee50
Martin Zenzes <martin.zenzes@dfki.de>
2014-01-10 13:37:30 +0100

    change directory-structure of xml-toolset for clearer layout
    some more changes accumulated during work... ;-)
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- src/Header.c
+++ src/Header.c
@@ -6,12 +6,12 @@
 #include "ndlcom_core/Header.h"
 
 /**
- * @brief The ndlcom-protocol defines the packet-coutner variable.
+ * @brief The ndlcom-protocol defines the packet-counter variable.
  *
- * it has to be increased for each packet transmissed to a receivind node -- regardless of
- * the payload used in the packet. therefore, a globally unique (static) table
- * is used to fill the correct packet counter. it is essentially an array
- * with one entry for each node, storing the last used packet counter.
+ * it has to be incremented for each packet transmitted to a receivind node -
+ * regardless of the payload used in the packet. Therefore, a globally unique
+ * (static) table is used to fill the correct packet counter. it is essentially
+ * an array with one entry for each node, storing the last used packet counter.
  *
  * @see include/ndlcom_core/Header.h
  */
