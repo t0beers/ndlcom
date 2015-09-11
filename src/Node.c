d355db9c7922253fe57b9aa7dde63ef323e964c6
Martin Zenzes <martin.zenzes@dfki.de>
2015-09-11 18:17:06 +0200

    Node: improve documentation a lot
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- src/Node.c
+++ src/Node.c
@@ -20,65 +20,74 @@ void ndlcomNodeInit(struct NDLComNode *node, struct NDLComBridge *bridge,
     /* calling this will also initialize the routing table */
     ndlcomNodeSetOwnSenderId(node, ownSenderId);
 
+    /* initialize handler which this Node is going to register in the bridge */
     ndlcomInternalHandlerInit(&node->myIdHandler, ndlcomNodeMessageHandler,
                               NDLCOM_INTERNAL_HANDLER_FLAGS_DEFAULT, node);
-
+    /* na, what did I say? */
     ndlcomBridgeRegisterInternalHandler(bridge, &node->myIdHandler);
 }
 
 void ndlcomNodeDeinit(struct NDLComNode *node) {
+    /* we do not want to be called anymore in the future */
     ndlcomBridgeDeregisterInternalHandler(node->bridge, &node->myIdHandler);
 }
 
-/* initializes internal structs with correct values. nearly resets the whole
- * state */
+/*
+ * Initializes internal structs with correct values.
+ */
 void ndlcomNodeSetOwnSenderId(struct NDLComNode *node,
                               const NDLComId ownSenderId) {
-
-    /* resets the packet-counters to use for each receiver, and is used to
-     * store our own deviceId at a convenient place */
+    /*
+     * resets the packet-counters to use for each receiver, and is used to
+     * store our own deviceId at a convenient place
+     */
     ndlcomHeaderPrepareInit(&node->headerConfig, ownSenderId);
-
-    /* we have to (re-)initialize the routing table, as we probably changed our
+    /*
+     * we have to (re-)initialize the routing table, as we probably changed our
      * position inside the network. this also gets rid of our new "own id",
-     * which might be already there */
+     * which might be already there
+     * 
+     * FIXME: This is wrong!
+     */
     ndlcomRoutingTableInit(&node->bridge->routingTable);
-
-    /**
+    /*
      * NOTE: putting our own "deviceId" into the routingtable. this is a hack,
      * to be able to detect messages going "to us" and not process them in the
      * outgoing side
      *
      * TODO: check that using "bridge" as the origin will work here...
      */
-    ndlcomRoutingTableUpdate(&node->bridge->routingTable, ownSenderId, node->bridge);
+    ndlcomRoutingTableUpdate(&node->bridge->routingTable, ownSenderId,
+                             node->bridge);
 }
 
 void ndlcomNodeMessageHandler(void *context, const struct NDLComHeader *header,
                               const void *payload) {
     struct NDLComNode *node = (struct NDLComNode *)context;
     const struct NDLComInternalHandler *internalHandler;
-
-    /* call the handlers which will see messages intended for "us" */
+    /*
+     * calls our own handlers only for messages directed at "us"
+     */
     if ((header->mReceiverId == node->headerConfig.mOwnSenderId) ||
         (header->mReceiverId == NDLCOM_ADDR_BROADCAST)) {
         list_for_each_entry(internalHandler, &node->internalHandlerList, list) {
-            /* internal interfaces will always see their own messages... */
+            /* internal interfaces will see their own broadcast messages... */
             internalHandler->handler(internalHandler->context, header, payload);
         }
     }
 }
 
-/* sending a payload to a receiver. message will be handled by internal handlers
+/*
+ * Sending a payload to a receiver
  */
 void ndlcomNodeSend(struct NDLComNode *node, const NDLComId receiverId,
                     const void *payload, const size_t payloadSize) {
-    /**
-     * preparation of the header
+    /*
+     * preparation of the header, filling in packet counter
      */
     struct NDLComHeader header;
     ndlcomHeaderPrepare(&node->headerConfig, &header, receiverId, payloadSize);
-    /**
+    /*
      * transmitting a message using the bridge
      */
     ndlcomBridgeSendRaw(node->bridge, &header, payload);
