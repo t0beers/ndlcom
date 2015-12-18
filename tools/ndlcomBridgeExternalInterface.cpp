1a17c2fd9e3fff3ba505733386b89dd638f5e772
Martin Zenzes <martin.zenzes@dfki.de>
2015-12-18 15:44:15 +0100

    ups
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/ndlcomBridgeExternalInterface.cpp
+++ tools/ndlcomBridgeExternalInterface.cpp
@@ -20,34 +20,6 @@
 #include <sys/select.h>
 #include <linux/limits.h>
 
-NDLComBridgeExternalInterface::NDLComBridgeExternalInterface(
-    NDLComBridge &_bridge, uint8_t flags)
-    : bridge(_bridge) {
-    ndlcomExternalInterfaceInit(
-        &external, NDLComBridgeExternalInterface::writeWrapper,
-        NDLComBridgeExternalInterface::readWrapper, flags, this);
-
-    ndlcomBridgeRegisterExternalInterface(&bridge, &external);
-}
-
-NDLComBridgeExternalInterface::~NDLComBridgeExternalInterface() {
-    ndlcomBridgeDeregisterExternalInterface(&bridge, &external);
-}
-
-void NDLComBridgeExternalInterface::writeWrapper(void *context, const void *buf,
-                                                 const size_t count) {
-    class NDLComBridgeExternalInterface *self =
-        static_cast<class NDLComBridgeExternalInterface *>(context);
-    self->writeEscapedBytes(buf, count);
-}
-
-size_t NDLComBridgeExternalInterface::readWrapper(void *context, void *buf,
-                                                  const size_t count) {
-    class NDLComBridgeExternalInterface *self =
-        static_cast<class NDLComBridgeExternalInterface *>(context);
-    return self->readEscapedBytes(buf, count);
-}
-
 NDLComBridgeStream::NDLComBridgeStream(NDLComBridge &_bridge, uint8_t flags)
     : ndlcom::ExternalInterfaceBase(_bridge, flags), fd_read(NULL),
       fd_write(NULL) {}
