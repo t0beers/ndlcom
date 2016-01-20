b8d32bf4a7db1d07d34f5fa7e3913895aa47b743
Martin Zenzes <martin.zenzes@dfki.de>
2016-01-20 15:19:48 +0100

    rename some variable to prevent name-clash in older "unistd.h"
    there, they define a variable "int pipe" with global visibility
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- src/ExternalInterfaceParseUri.cpp
+++ src/ExternalInterfaceParseUri.cpp
@@ -2,11 +2,11 @@
 
 #include "ndlcom/ExternalInterface.hpp"
 
-const std::string serial = "serial://";
-const std::string udp = "udp://";
-const std::string pipe = "pipe://";
-const std::string fpga = "fpga://";
-const std::string pty = "pty://";
+const std::string uri_prefix_serial = "serial://";
+const std::string uri_prefix_udp = "udp://";
+const std::string uri_prefix_pipe = "pipe://";
+const std::string uri_prefix_fpga = "fpga://";
+const std::string uri_prefix_pty = "pty://";
 
 /**
  * TODO:
@@ -23,8 +23,9 @@ ndlcom::ParseUriAndCreateExternalInterface(std::ostream &out,
                                            struct NDLComBridge &bridge,
                                            std::string uri, uint8_t flags) {
 
-    if (uri.compare(0, serial.length(), serial) == 0) {
-        size_t begin_device = uri.find(serial) + serial.size();
+    if (uri.compare(0, uri_prefix_serial.length(), uri_prefix_serial) == 0) {
+        size_t begin_device =
+            uri.find(uri_prefix_serial) + uri_prefix_serial.size();
         size_t begin_baud = uri.find_last_of(":") + 1;
         std::string device(
             uri.substr(begin_device, begin_baud - begin_device - 1));
@@ -39,8 +40,9 @@ ndlcom::ParseUriAndCreateExternalInterface(std::ostream &out,
             << "baud\n";
         return new ExternalInterfaceSerial(bridge, device, baudrate, flags);
 
-    } else if (uri.compare(0, udp.length(), udp) == 0) {
-        size_t begin_hostname = uri.find(udp) + udp.size();
+    } else if (uri.compare(0, uri_prefix_udp.length(), uri_prefix_udp) == 0) {
+        size_t begin_hostname =
+            uri.find(uri_prefix_udp) + uri_prefix_udp.size();
         size_t begin_inport = uri.find(":", begin_hostname) + 1;
         size_t begin_outport = uri.find(":", begin_inport) + 1;
         std::string hostname(
@@ -65,22 +67,24 @@ ndlcom::ParseUriAndCreateExternalInterface(std::ostream &out,
         return new ExternalInterfaceUdp(bridge, hostname, inport, outport,
                                         flags);
 
-    } else if (uri.compare(0, pipe.length(), pipe) == 0) {
-        size_t begin_pipename = uri.find(pipe) + pipe.size();
+    } else if (uri.compare(0, uri_prefix_pipe.length(), uri_prefix_pipe) == 0) {
+        size_t begin_pipename =
+            uri.find(uri_prefix_pipe) + uri_prefix_pipe.size();
         std::string pipename(uri.substr(begin_pipename));
         out << "opening pipe '" << pipename << "'\n";
         return new ExternalInterfacePipe(bridge, pipename, flags);
 
-    } else if (uri.compare(0, fpga.length(), fpga) == 0) {
-        size_t begin_fpganame = uri.find(fpga) + fpga.size();
+    } else if (uri.compare(0, uri_prefix_fpga.length(), uri_prefix_fpga) == 0) {
+        size_t begin_fpganame =
+            uri.find(uri_prefix_fpga) + uri_prefix_fpga.size();
         std::string fpganame(uri.substr(begin_fpganame));
         if (fpganame.empty())
             fpganame = "/dev/NDLCom";
         out << "opening fpga '" << fpganame << "'\n";
         return new ExternalInterfaceFpga(bridge, fpganame);
 
-    } else if (uri.compare(0, pty.length(), pty) == 0) {
-        size_t begin_ptyname = uri.find(pty) + pty.size();
+    } else if (uri.compare(0, uri_prefix_pty.length(), uri_prefix_pty) == 0) {
+        size_t begin_ptyname = uri.find(uri_prefix_pty) + uri_prefix_pty.size();
         std::string ptyname(uri.substr(begin_ptyname));
         out << "opening pty master '" << ptyname << "'\n";
         return new ExternalInterfacePty(bridge, ptyname);
