99125c1c69d627b0552ce332444f4d2cacb62050
Martin Zenzes <martin.zenzes@dfki.de>
2017-09-08 16:01:10 +0200

    Payload: add time_point "receivedAt" to IncomingPayload
    restructure the whole hierarchy
    
    also fix a nasty "missing forward decl" leading to differing pointer
    types for "NDLComExternalInterface"... man... makes sense after
    understanding this, but man...
    
    returning "void*" instead of "uint8_t*"...
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- include/ndlcom/Payload.hpp
+++ include/ndlcom/Payload.hpp
@@ -3,75 +3,144 @@
 
 #include "ndlcom/Types.h"
 
-#include <cstddef>
+#include <chrono>
 #include <vector>
+
+/** forward decl to be able to use the origin-pointer in IncomingPayload: */
+struct NDLComExternalInterface;
 
 namespace ndlcom {
 
 /**
- * This is a small wrapper class to provide an easy contiguous region of
- * dynamically sized memory without having to write copy and assignment
+ * @brief Wrapped continous memory to represent a payload of an ndlcom packet
+ *
+ * This is a small wrapper base-class to provide an easy contiguous region of
+ * quasi-dynamically sized memory without having to write copy or assignment
  * constructors.
- */
-struct Payload : private std::vector<uint8_t> {
-    Payload(const void *payload, const size_t size);
-    /**
-     * only these three "properties" of the actual std::vector are exported,
-     * all the rest is deliberately kept private:
-     */
-    size_t size() const;
-    uint8_t *data();
-    const uint8_t *data() const;
-};
-
-/**
- * This object contains a NDLComHeader, as it was observed on the wire for
- * this packet, and the respective payload. Additionally, the pointer to the
- * originating interface is stored for later reference.
  *
- * The payload is decoded and ready to be used by a consumer. An
- * IncomingPacket shall never be changed after it came to life, thus all the
- * members are const.
+ * Internally the data of the inherited std::vector is filled in the ctor of
+ * this class when passing the void-pointer and size argument, as iterator
+ * "first" and "last". This results in reserving exactly as much memory when
+ * copying the bytes behind the pointer into the std::vector, not a single byte
+ * more. As the payload-size shall never change after construction this is a
+ * nice for-free optimization.
  *
- * TODO: Maybe an std::chrono::time_point as well?
+ * Please be sure to only put "size" arguments of sensible type into the ctor,
+ * the implicit cast of NDLComDataLen might suprise you. But it would happen
+ * anyways, as an ndlcom-packet only carries a maximum of 255 bytes. An extra
+ * size-check could be added, but meh...
  */
-struct IncomingPayload {
-    IncomingPayload(const struct NDLComHeader *_header, const void *_payload,
-                    const struct NDLComExternalInterface *);
-    const struct NDLComHeader header;
-    const struct Payload payload;
-    const struct NDLComExternalInterface* origin;
-};
-
-/**
- * The OutgoingPacket has a destinationId and a number of bytes intended to be
- * used as payload.
- */
-struct OutgoingPayload {
-    OutgoingPayload(const NDLComId _receiverId, const void *_payload,
-                    const size_t size);
-    const NDLComId receiverId;
+struct Payload : protected std::vector<uint8_t> {
     /**
-     * modifiable, as the content of payload will be changed by
-     * "represupport::send()" before actually sending.
+     * Copies the memory region of "payload" until "payload+size" into the
+     * inherited std::vector.
      *
-     * FIXME: this is a hard sell... where to store the "mBase.mId" to give
-     * into "represupport::send()"?
+     * Could make this ctor "explicit"...
      */
-    struct Payload payload;
+    Payload(const void *payload, NDLComDataLen size);
+    /**
+     * Only these "properties" of the actual std::vector interface are
+     * exported, all the rest is deliberately kept hidden by using "protected"
+     * when inheriting.
+     */
+    NDLComDataLen dataLen() const;
+    /**
+     * This allows to access the internal data for reading... deliberately only
+     * as a "const pointer", so that nobody (tm) can change the data once it
+     * was initialized.
+     *
+     * Implementation detail: Later deriving classes can choose to provide a
+     * non-const accessor function, see represupport::TypedOutgoingPayload for
+     * example.
+     *
+     * Internally the data is stored in std::vector<uint8_t>, but casted to a
+     * void pointer by this accessor.
+     */
+    const void* data() const;
 };
 
 /**
+ * @brief Wrapped payload+header of an ndlcom packet
  *
- * design decisions:
+ * An ndlcom::RawPayload is used to represent a complete ndlcom packet with
+ * full control over the appereance. This can be used either when sending or to
+ * store received messages.
  *
- * - use new/delete on an internal raw pointer...  more hassle to write the
- *   ctors correctly. seems to not be noticably faster than a non-changing
+ * Note that, normally, a message header is constructed by an ndlcom::Node
+ * after consulting internal state. The normal usecase would be, after mixing
+ * in some more properties, to be the base of an ndlcom::IncomingPayload, see
+ * below.
+ */
+struct RawPayload : public ndlcom::Payload {
+    RawPayload(const struct NDLComHeader *_header, const void *_payload)
+        : Payload(_payload, _header->mDataLen), header(*_header) {}
+    /**
+     * After creation, this shall never be changed again. But when this is a
+     * "const" member, there will be no default assignment operator -- which we
+     * did not want to implement in the first place... what a conundrum...
+     */
+    struct NDLComHeader header;
+};
+
+/**
+ * @brief Wrapped ndlcom packet after it was received by an ndlcom::Bridge
+ *
+ * It contains a NDLComHeader, as it was observed on the wire for this packet,
+ * and the respective payload. Additionally, the pointer to the originating
+ * NDLComExternalInterface is stored for later reference as well as the point
+ * in time, when this packet was received.
+ *
+ * The payload is decoded and ready to be used by a consumer. An IncomingPacket
+ * shall never be changed after it came to life, thus all the members are
+ * const.
+ */
+struct IncomingPayload : public ndlcom::RawPayload {
+    IncomingPayload(const struct NDLComHeader *_header, const void *_payload,
+                    const struct NDLComExternalInterface *,
+                    std::chrono::time_point<std::chrono::system_clock>
+                        _receivedAt = std::chrono::system_clock::now());
+    /**
+     * The interface where this message came from.
+     *
+     * NOTE: This can also be nullptr, in case the message was sent by some
+     * component of the ndlcom::Bridge itself.
+     */
+    const struct NDLComExternalInterface *origin;
+    /**
+     * A Timestamp
+     *
+     * Is it better to fill it in our ctor than using the passed-in one?
+     */
+    std::chrono::time_point<std::chrono::system_clock> receivedAt;
+};
+
+/**
+ * @brief Wrapper for an outgoing packet, before it was actually sent
+ *
+ * The OutgoingPacket has an NDLComId as a destination and a number of bytes
+ * intended to be used as payload.
+ *
+ * It has no header, as this will be created and filled by the sending
+ * instance, possibly derived from ndlcom::Node.
+ */
+struct OutgoingPayload : public ndlcom::Payload {
+    OutgoingPayload(NDLComId _destinationId, const void *_payload, size_t size);
+    /**
+     * Where this message shall be sent to
+     */
+    NDLComId destinationId;
+};
+
+/**
+ * Notes on some design decisions during writing of this family of classes:
+ *
+ * - do not use new/delete on an internal raw pointer...  more hassle to write
+ *   the ctors correctly. seems to not be noticably faster than a non-changing
  *   std::vector.
  * - smart pointers: might have the benefit to avoid unnessary copying as the
- *   data can be shared safely... how often does this happen? and results in
- *   more complex code. on the other hand: just wrap the resulting "packet" in
- *   a smart pointer and we have the same effect...
+ *   data can be shared safely... if locked properly... how often does this
+ *   happen? and results in more complex code. on the other hand: just wrap the
+ *   resulting "Payload" object in a smart pointer and we have the same effect...
  * - wrap std::vector internally? don't need its iterators... dont need its
  *   resizing...  would not have to write the ctors... seems to be reasonable
  *   fast when not resizing... privately deriving feels even better
--- src/Payload.cpp
+++ src/Payload.cpp
@@ -2,21 +2,22 @@
 
 using namespace ndlcom;
 
-Payload::Payload(const void *payload, const size_t size)
+Payload::Payload(const void *payload, const NDLComDataLen size)
     : std::vector<uint8_t>((const uint8_t *)payload,
                            (const uint8_t *)payload + size) {}
 
-size_t Payload::size() const { return std::vector<uint8_t>::size(); }
+NDLComDataLen Payload::dataLen() const { return std::vector<uint8_t>::size(); }
 
-uint8_t *Payload::data() { return std::vector<uint8_t>::data(); }
+const void *Payload::data() const {
+    return static_cast<const void *>(std::vector<uint8_t>::data());
+}
 
-const uint8_t *Payload::data() const { return std::vector<uint8_t>::data(); }
+IncomingPayload::IncomingPayload(
+    const struct NDLComHeader *_header, const void *_payload,
+    const struct NDLComExternalInterface *_origin,
+    std::chrono::time_point<std::chrono::system_clock> _receivedAt)
+    : RawPayload(_header, _payload), origin(_origin), receivedAt(_receivedAt) {}
 
-IncomingPayload::IncomingPayload(const struct NDLComHeader *_header,
-                                 const void *_payload,
-                                 const struct NDLComExternalInterface *_origin)
-    : header(*_header), payload(_payload, _header->mDataLen), origin(_origin) {}
-
-OutgoingPayload::OutgoingPayload(const NDLComId _receiverId,
-                                 const void *_payload, const size_t size)
-    : receiverId(_receiverId), payload(_payload, size) {}
+OutgoingPayload::OutgoingPayload(NDLComId _destinationId, const void *_payload,
+                                 size_t size)
+    : ndlcom::Payload(_payload, size), destinationId(_destinationId) {}
